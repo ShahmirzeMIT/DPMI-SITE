81092acd66f479703c715405f47ce211c8d9a708
Shahmirze <85016768+ShahmirzeMIT@users.noreply.github.com>
2024-12-20 10:52:03 +0300

    Update HomePagePart2.tsx

--- src/pages/SecondHome/HomePagePart2.tsx
+++ src/pages/SecondHome/HomePagePart2.tsx
@@ -1,19 +1,19 @@
 import { Box } from '@mui/material'
 
 import FirstCourse from './FirstCourse'
-// import SecondCourse from './SecondCourse'
-// import ThirdCourse from './ThirdCourse'
-// import FourthCourse from './Fourth'
-// import AboutCourses from '../Home/AboutCourses'
+import SecondCourse from './SecondCourse'
+import ThirdCourse from './ThirdCourse'
+import FourthCourse from './Fourth'
+import AboutCourses from '../Home/AboutCourses'
 
 export default function HomePagePart2() {
   return (
     <Box>
         <FirstCourse/>
-        {/* <SecondCourse/>
+        <SecondCourse/>
         <ThirdCourse/>
         <FourthCourse/>
-        <AboutCourses/> */}
+        <AboutCourses/>
     </Box>
   )
 }
